3de56ec3e1bcf83699355cc17a87af3566972628
William Dias <smdwilliam89@gmail.com>
2022-04-09 13:15:01 +0100

    estruturando html em novo projeto

---
+++ 07.base-apparel-coming-soon/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import Logo from './Components/Logo';
+import Intro from './Components/Intro';
+// import Form from './Components/Form';
+// import Img from './Components/Img';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Logo /> */}
+      <Intro />
+      {/* <Form /> */}
+      {/* <Img /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ 07.base-apparel-coming-soon/src/Components/Intro/index.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+function Intro() {
+    return (
+        <div className="container">
+            <div className="title">
+                <h1><span>We're</span> <br/>coming soon</h1>
+            </div>
+
+            <div>
+                <p>
+                Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default Intro;
+
