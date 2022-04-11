ca36f916dd766462246172b3ce04df4796b9ad15
William Dias <smdwilliam89@gmail.com>
2022-04-11 15:55:35 +0100

    sei la

--- 07.base-apparel-coming-soon/src/Components/Form/index.jsx
+++ 07.base-apparel-coming-soon/src/Components/Form/index.jsx
@@ -1,20 +1,21 @@
-import React, { Component } from "react";
-import "./style.css";
-import IconArrow from "./icon-arrow.svg";
+import React from 'react';
+import './style.css';
+import IconArrow from './icon-arrow.svg';
 
-class Form extends Component {
-  render() {
-    return (
-      <div>
-        <form className='form'>
-          <input name='email' type='email' placeholder='E-mail Address' />
-          <button>
-            <img src={IconArrow} alt='Icon Arrow' className='icon-arrow' />
-          </button>
-        </form>
-      </div>
-    );
+function Form() {
+  function handleSubmit(e) {
+    e.preventdefault();
+    console.log('You clicked submit.');
   }
+
+  return (
+    <form onSubmit={handleSubmit} className='form'>
+      <input name='email' placeholder='E-mail Address' />
+      <button type='submit'>
+        <img src={IconArrow} alt='Icon Arrow' className='icon-arrow' />
+      </button>
+    </form>
+  );
 }
 
 export default Form;
