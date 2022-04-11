75dcbaf4d45dcdfe038f380a0635a848443d1a31
William Dias <smdwilliam89@gmail.com>
2022-04-11 22:35:44 +0100

    Tentando inserir ícone de erro com JS

--- 08.intro-component/js/index.js
+++ 08.intro-component/js/index.js
@@ -13,9 +13,64 @@ form.onsubmit = function (e) {
 
     let span = inputName.nextSibling.nextSibling;
     span.innerText = 'First Name cannot be empty';
+
+    let icon = document.querySelector('.iconError');
+    icon.style.visibility = 'visible';
   } else {
     inputName.classList.remove('inputError');
     let span = inputName.nextSibling.nextSibling;
+    span.innerText = '';
+    let icon = document.querySelector('.iconError');
+    icon.style.visibility = 'hidden';
+  }
+
+  let inputLastName = document.forms['subscribeForm']['last-name'];
+
+  if (!inputLastName.value) {
+    hasError = true;
+    inputLastName.classList.add('inputError');
+
+    let span = inputLastName.nextSibling.nextSibling;
+    span.innerText = 'Last Name cannot be empty';
+
+    let icon = document.querySelector('.iconError');
+    icon.style.visibility = 'visible';
+  } else {
+    inputLastName.classList.remove('inputError');
+    let span = inputLastName.nextSibling.nextSibling;
+    span.innerText = '';
+
+    let icon = document.querySelector('.iconError');
+    icon.style.visibility = 'hidden';
+  }
+
+  let inputEmail = document.forms['subscribeForm']['email'];
+
+  if (!inputEmail.value) {
+    hasError = true;
+    inputEmail.classList.add('inputError');
+
+    let span = inputEmail.nextSibling.nextSibling;
+    span.innerText = 'E-mail cannot be empty';
+    icon.style.visibility = 'visible';
+  } else {
+    inputEmail.classList.remove('inputError');
+    let span = inputEmail.nextSibling.nextSibling;
+    span.innerText = '';
+  }
+
+  let inputPassword = document.forms['subscribeForm']['password'];
+
+  if (!inputPassword.value) {
+    hasError = true;
+    inputPassword.classList.add('inputError');
+
+    let span = inputPassword.nextSibling.nextSibling;
+    span.innerText = 'Password cannot be empty';
+    icon.style.visibility = 'visible';
+  } else {
+    inputPassword.classList.remove('inputError');
+    let span = inputPassword.nextSibling.nextSibling;
     span.innerText = '';
   }
 };
